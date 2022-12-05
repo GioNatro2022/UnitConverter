7b3673ae8dd3f168e41edbe70a1796536f4934f0
GioNatro2022 <giorgin@pangea-it.com>
2022-12-06 03:05:44 +0400

    test changes

---
+++ UnitConverter/Converters/Temperature/CelsiusToFarenheitConverter.cs
@@ -0,0 +1,32 @@
+﻿using UnitConverter.Base;
+
+namespace UnitConverter.Converters.Temperature
+{
+    public class CelsiusToFarenheitConverter : BaseConverter
+    {
+
+        public CelsiusToFarenheitConverter()
+        {
+            conversionLogic = new CelsiusToFarenheitConversion();
+            fromValueSynonims = new List<string> {"celsius", "celsiuses", "c" };
+            toValueSynonims = new List<string> { "farenheit", "farenheits", "f" };
+        }
+        protected override List<string> fromValueSynonims { get; set; }
+        protected override List<string> toValueSynonims { get; set; }
+        protected override BaseConversion conversionLogic { get; set; }
+    }
+
+    public class CelsiusToFarenheitConversion : BaseConversion
+    {
+        public override decimal LeftToRightConversion(decimal value, decimal UnitModifier = 1)
+        {
+            return Math.Round((value* 9/5)+32, 6);
+        }
+
+        public override decimal RightToLeftConversion(decimal value, decimal UnitModifier = 1)
+        {
+            return Math.Round((value -32) * 5/9, 6);
+
+        }
+    }
+}
